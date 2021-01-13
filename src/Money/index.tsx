b5ef22145c8a0cfb5565226a862995b7984f3be4
zhuangjinan <zhuangjinan2018@gmail.com>
2021-01-13 17:34:15 +0800

    eject

--- src/Money/index.tsx
+++ src/Money/index.tsx
@@ -1,3 +1,14 @@
+import { ReactComponent as TestIcon } from "assets/svg/test1.svg";
+import { SvgIcon } from "@material-ui/core";
 export default function Money() {
-  return <div>Money</div>;
+  return (
+    <div>
+      Money
+      <SvgIcon
+        component={TestIcon}
+        viewBox="0 0 1024 1024"
+        style={{ fontSize: 200 }}
+      ></SvgIcon>
+    </div>
+  );
 }
